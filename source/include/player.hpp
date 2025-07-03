657e1f6a45e716eef2f1c7060fa9a3308b6b3210
Luisa <luisaferreirass08@gmail.com>
2025-07-03 17:03:52 -0300

    feat: implementing player abstract class

---
+++ source/include/player.hpp
@@ -0,0 +1,18 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include "snake.hpp"
+
+enum player_type_e : std::uint8_t { RANDOM = 0, BACKTRACKING, HUMAN };
+
+class SPlayer {
+public:
+  SPlayer() = default;
+  virtual ~SPlayer() = default;
+  virtual MoveDir next_move() = 0;
+  virtual player_type_e type() const = 0;
+  virtual bool path_finder() { return false; };  //??
+};
+
+
+#endif
